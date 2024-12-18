b95443a5f4a4616d0600f6e3568e70cd5ba8ce22
JESS IZEN <jlizen@amazon.com>
2024-12-18 18:54:44 +0000

    strip out conditional compilation

--- src/lib.rs
+++
@@ -1,7 +0,0 @@
-pub fn test()-> &'static str {
-    #[cfg(compute_heavy_tokio_executor_multithreaded)]
-    return "multithreaded";
-
-    #[cfg(not(compute_heavy_tokio_executor_multithreaded))]
-    return "singlethreaded"
-}
