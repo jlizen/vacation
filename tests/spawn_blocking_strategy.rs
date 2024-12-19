79f99cb1030fbaf4d7b7e4e34b5842eaa87cc13f
jlizen <jackliebiz@gmail.com>
2024-12-19 22:37:42 +0000

    tweak tests

--- tests/spawn_blocking_strategy.rs
+++ tests/spawn_blocking_strategy.rs
@@ -1,10 +1,10 @@
-use compute_heavy_future_executor::{
-    initialize_spawn_blocking_strategy, spawn_compute_heavy_future,
-};
-
 #[cfg(feature = "tokio")]
 #[tokio::test]
 async fn spawn_blocking_strategy() {
+    use compute_heavy_future_executor::{
+        initialize_spawn_blocking_strategy, spawn_compute_heavy_future,
+    };
+
     initialize_spawn_blocking_strategy();
 
     let future = async { 5 };
