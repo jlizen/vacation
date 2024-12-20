bfc048f1c528ba9010ec4a6097a6c283488fb40e
jlizen <jackliebiz@gmail.com>
2024-12-20 00:12:08 +0000

    make everything but block_in_place cancellable, add cancellable tests

--- tests/block_in_place_default.rs
+++ tests/block_in_place_default.rs
@@ -1,6 +1,6 @@
-#[cfg(not(feature = "tokio"))]
-#[tokio::test]
-async fn default_to_current_context_non_tokio() {
+#[cfg(feature = "tokio_multi_threaded")]
+#[tokio::test(flavor = "multi_thread")]
+async fn default_to_current_context_tokio_multi_threaded() {
     use compute_heavy_future_executor::spawn_compute_heavy_future;
 
     let future = async { 5 };
