582f94583da5888aa752af0dc18b57cdbfad529e
jlizen <jackliebiz@gmail.com>
2024-12-28 23:35:29 +0000

    add stdError impl to Error type, add Send + Sync + `static bounds

--- src/error.rs
+++ src/error.rs
@@ -40,3 +40,12 @@ impl fmt::Display for Error {
         }
     }
 }
+
+impl std::error::Error for Error {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        match self {
+            Self::BoxError(err) => Some(err.source()?),
+            _ => None
+        }
+    }
+}
