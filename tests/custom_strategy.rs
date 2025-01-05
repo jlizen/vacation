9403b18f20dc93d55b9bbdbdc3195a2a4c50539d
jlizen <jackliebiz@gmail.com>
2025-01-05 09:52:41 +0000

    fix race conditions in concurrency tests due to tasks being spawned to (quicker) test runtime handle

--- tests/custom_strategy.rs
+++ tests/custom_strategy.rs
@@ -2,11 +2,15 @@ use std::{sync::OnceLock, time::Duration};
 
 use futures_util::future::join_all;
 use rayon::ThreadPool;
-use vacation::{execute, init, ChanceOfBlocking, CustomClosureInput, CustomClosureOutput};
+use vacation::{
+    execute, init, ChanceOfBlocking, CustomClosureInput, CustomClosureOutput, ExecutorStrategy,
+    GlobalStrategy,
+};
 
 static THREADPOOL: OnceLock<ThreadPool> = OnceLock::new();
 
-fn initialize() {
+#[tokio::test]
+async fn custom_concurrency() {
     THREADPOOL.get_or_init(|| rayon::ThreadPoolBuilder::default().build().unwrap());
 
     let custom_closure = |f: CustomClosureInput| {
@@ -16,28 +20,16 @@ fn initialize() {
         }) as CustomClosureOutput
     };
 
-    let _ = init()
+    init()
         .max_concurrency(3)
         .custom_executor(custom_closure)
-        .install();
-}
+        .install()
+        .unwrap();
 
-#[tokio::test]
-async fn custom_strategy() {
-    initialize();
-
-    let closure = || {
-        std::thread::sleep(Duration::from_millis(15));
-        5
-    };
-
-    let res = execute(closure, ChanceOfBlocking::High).await.unwrap();
-    assert_eq!(res, 5);
-}
-
-#[tokio::test]
-async fn custom_concurrency() {
-    initialize();
+    assert_eq!(
+        vacation::global_strategy(),
+        GlobalStrategy::Initialized(ExecutorStrategy::Custom)
+    );
 
     let start = std::time::Instant::now();
 
@@ -47,17 +39,16 @@ async fn custom_concurrency() {
         std::thread::sleep(Duration::from_millis(15));
         5
     };
-    tokio::time::sleep(Duration::from_millis(5)).await;
 
     // note that we also are racing against concurrency from other tests in this module
     for _ in 0..6 {
         futures.push(execute(closure, ChanceOfBlocking::High));
     }
 
-    join_all(futures).await;
+    let res = join_all(futures).await;
 
     let elapsed_millis = start.elapsed().as_millis();
     assert!(elapsed_millis < 50, "futures did not run concurrently");
-
     assert!(elapsed_millis > 20, "futures exceeded max concurrency");
+    assert!(!res.iter().any(|res| res.is_err()));
 }
